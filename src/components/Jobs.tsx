feab87a09a3e6f9135b7eaf20babd34c728721e1
NikaNishnianidze <nikanishno8@gmail.com>
2025-04-16 19:05:27 +0400

    add filter div

--- src/components/Jobs.tsx
+++ src/components/Jobs.tsx
@@ -3,16 +3,63 @@ import searchIcon from "../../public/assets/desktop/icon-search.svg";
 import data from "../data.json";
 import { useNavigate } from "react-router-dom";
 import { JobInfo } from "../Types";
+import { useState } from "react";
+import locationIcon from "../../public/assets/desktop/icon-location.svg";
 
 const Jobs = () => {
   const navigate = useNavigate();
+
+  const [filterOpen, setFilterOpen] = useState<boolean>(false);
 
   const handleInfo = (job: JobInfo) => {
     navigate(`/home/${job.id}`);
   };
 
+  const toggleFilter = () => {
+    setFilterOpen(true);
+  };
+
   return (
     <div className="flex flex-col items-center ">
+      {filterOpen && (
+        <div
+          onClick={() => setFilterOpen(false)}
+          className="fixed inset-0 z-50 bg-black/50 flex items-center justify-center"
+        >
+          <div
+            onClick={(e) => e.stopPropagation()}
+            className="box w-[327px] bg-input rounded-[6px] dark:bg-input-dark"
+          >
+            <div className="div-input flex items-center gap-[16px] p-[24px]">
+              <img src={locationIcon} alt="location icon" />
+              <input
+                type="text"
+                placeholder="Filter by location…"
+                className="text-[#19202D] text-[16px] font-normal outline-none dark:text-[#fff]"
+              />
+            </div>
+            <div className="divider w-[327px] h-[1px] bg-oval/20 mt-[14px]"></div>
+            <div className="checkbox-div mt-[27px] px-[24px] flex items-center gap-[16px]">
+              <input
+                type="checkbox"
+                id="time"
+                className="appearance-none checked:bg-site focus:outline-none w-[24px] h-[24px] rounded-[3px] accent-site cursor-pointer bg-input-dark/10 dark:bg-white/10"
+              />
+              <label
+                htmlFor="time"
+                className="text-[#19202D] text-[16px] font-bold dark:text-[#fff]"
+              >
+                Full Time Only
+              </label>
+            </div>
+            <div className="search-div mt-[24px] flex flex-col items-center mb-[24px]">
+              <button className="w-[279px] py-[16px] rounded-[5px] text-[#fff] font-bold text-[16px] bg-site">
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
       <div className="filter-div w-[327px]  flex items-center relative bottom-7">
         <input
           type="text"
@@ -24,6 +71,7 @@ const Jobs = () => {
             src={filterIcon}
             alt="filter icon"
             className="dark:filter dark:brightness-0 dark:invert"
+            onClick={toggleFilter}
           />
           <div className="w-[48px] h-[48px] rounded-[6px] flex flex-col items-center justify-center bg-search-icon ">
             <img
